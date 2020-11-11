0052deee6327635b510b145c7205cbe8ec6e9272
wolfadex <wolfadex@gmail.com>
2020-11-10 19:58:50 -0700

    Add a basic query

--- src/Api/Database.elm
+++ src/Api/Database.elm
@@ -1,4 +1,4 @@
-module Api.Database exposing (mutate)
+module Api.Database exposing (mutate, query)
 
 import Http exposing (Error(..), Response(..))
 import Json.Decode exposing (Decoder)
@@ -16,6 +16,46 @@ mutate mutation decoder =
             [ ( "query"
               , "mutation {"
                     ++ mutation
+                    ++ "}"
+                    |> Json.Encode.string
+              )
+            ]
+                |> Json.Encode.object
+                |> Http.jsonBody
+        , resolver =
+            Http.stringResolver
+                (\resp ->
+                    case resp of
+                        BadUrl_ url ->
+                            Err (BadUrl url)
+
+                        Timeout_ ->
+                            Err Timeout
+
+                        NetworkError_ ->
+                            Err NetworkError
+
+                        BadStatus_ metadata body ->
+                            Err (BadStatus metadata.statusCode)
+
+                        GoodStatus_ metadata body ->
+                            Json.Decode.decodeString decoder body
+                                |> Result.mapError (Json.Decode.errorToString >> BadBody)
+                )
+        , timeout = Nothing
+        }
+
+
+query : String -> Decoder a -> Task Error a
+query qry decoder =
+    Http.task
+        { method = "POST"
+        , headers = []
+        , url = "http://localhost:8080/graphql"
+        , body =
+            [ ( "query"
+              , "query {"
+                    ++ qry
                     ++ "}"
                     |> Json.Encode.string
               )
